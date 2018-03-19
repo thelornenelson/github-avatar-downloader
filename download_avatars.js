ed1dba4eaad55da959d9420b61050c5716bcfa10
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-19 23:38:42 +0000

    added a variety of error handling

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,7 @@ var request = require('request');
 var fs = require('fs');
 
 // this loads environment variables, in this case the GITHUB_TOKEN
-require('dotenv').config();
+var dotenv = require('dotenv').config();
 
 // retrieves list of GitHub repo contributors for <repoOwner>/<repoName> and executes
 // cb function on returned body (body is JSON array of objects corresponding to contributors)
@@ -11,14 +11,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request',
-      'Authorization': 'token ' + process.env.GITHUB_TOKEN
+    'User-Agent': 'request',
     }
   };
 
+  if(process.env.GITHUB_TOKEN){
+    options.headers.Authorization = 'token ' + process.env.GITHUB_TOKEN;
+  }
+
   request(options, function(err, result, body){
-    cb(err, body);
-  })
+    if(err){
+      console.log(err);
+    } else {
+      if(result.statusCode == '404'){
+        console.log("Unable to find /" + repoOwner + "/" + repoName);
+      } else {
+        //check response code, if 404 then log "unable to find user/repo";
+        cb(err, body);
+      }
+    }
+  });
 }
 
 // downloads and saves image stored at url to local filePath.
@@ -27,23 +39,32 @@ function downloadImageByURL(url, filePath) {
     url: url,
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token ' + process.env.GITHUB_TOKEN
     }
   };
+
+  if(process.env.GITHUB_TOKEN){
+    options.headers.Authorization = 'token ' + process.env.GITHUB_TOKEN;
+  }
 
   request.get(options)
   .on('error', function(err){
     throw err;
   })
+  .on('end', function(){
+    console.log("Downloaded " + url);
+  })
   .pipe(fs.createWriteStream(filePath));
 }
 
+// callback function to save Avatars to local disk.
 var saveAvatars = function(err, result){
   // set directory to save avatars
   var avatarDir = "./avatars/";
 
-  // log errors, or null if no errors
-  console.log("Errors: ", err);
+  // log errors, if any
+  if(err){
+    console.log("Errors: ", err);
+  }
 
   // parse the results string
   var results = JSON.parse(result);
@@ -59,22 +80,24 @@ var saveAvatars = function(err, result){
       // if directory read was successful, directory already exists
       // iterate over results and download and save images
       results.forEach(function(element){
-        console.log("Avatar URL for " + element.login + " is " + element.avatar_url);
+        // console.log("Avatar URL for " + element.login + " is " + element.avatar_url);
 
         // download and save avatar, assuming jpg format
         downloadImageByURL(element.avatar_url, avatarDir + element.login + ".jpg");
       });
     }
-
   });
 };
+
+// check to confirm .env loaded correctly
+if(dotenv.error){
+  console.log(".env file missing or invalid. Let's try to continue without authorization.");
+}
 
 
 // check to make sure 2 inputs were read from command line.
 if(process.argv[2] && process.argv[3]){
-
   getRepoContributors(process.argv[2], process.argv[3], saveAvatars);
-
 } else {
   console.log("Error: include repo owner and repo name");
 }
