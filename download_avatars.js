0747e5ff30109885e59a38d402218096021d0056
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-19 20:32:21 +0000

    implemented image saving

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secrets = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -15,11 +16,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   })
 }
 
+function downloadImageByURL(url, filePath) {
+  var options = {
+    url: url,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request.get(options)
+  .on('error', function(err){
+    throw err;
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
+
 getRepoContributors("nodejs", "node", function(err, result){
+  // log errors, or null if no errors
   console.log("Errors: ", err);
+
+  // parse the results string
   var results = JSON.parse(result);
+
+  // iterate over results and output avatar urls
   results.forEach(function(element){
     console.log("Avatar URL for " + element.login + " is " + element.avatar_url);
+
+    // download and save avatar, assuming jpg format
+    downloadImageByURL(element.avatar_url, "avatars/" + element.login + ".jpg");
   });
 
 });
