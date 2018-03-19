0c31504be77a0f5113c8cfd77454b9bc468ded79
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-19 20:10:40 +0000

    added token authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,18 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, result, body){
+    cb(err, body);
+  })
 }
 
 getRepoContributors("nodejs", "node", function(err, result){
